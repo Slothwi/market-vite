7a342ef133d0e2aac933143e99ddea5f7d1b6f84
Paula Marti <paula0173@hotmail.com>
2024-02-28 19:59:15 -0300

    Actualizacion de codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
-// import { Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-// import Nav from './Components/Navbar'
-// import Home from './Views/Home'
-// import Sidebar from './Components/Sidebar'
-// import Access from './Views/Access'
-// import Cart from './Views/Cart'
-// import Favs from './Views/Favoritos'
-// import Profile from './Views/Profile'
-// import Publicar from './Views/Publicar'
-// import Registro from './Views/registro'
-// import Ventas from './Views/Ventas'
-// import Historial from './Views/Historial'
-// import MainPage from './Components/MainPage'
-// import { Outlet } from "react-router-dom";
-// import { Container } from "react-bootstrap";
 
 import Themeroutes from "./routes/Router";
 import { useRoutes } from "react-router-dom";
