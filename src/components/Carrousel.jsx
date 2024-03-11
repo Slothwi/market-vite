e65dd37eab19a32633143de8fb126ca83c6fa3f2
Slothwi <gabo.leonp@gmail.com>
2024-03-11 20:26:21 -0300

    Act Carrousel

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -5,16 +5,16 @@ const Carrousel = () => {
     return (
         <Carousel className='mt-3'>
             <Carousel.Item interval={1000}>
-                <CarouselImage imgUrl="../src/assets/carrusel1.png" />
+                <CarouselImage imgUrl="../public/carrusel1.png" />
             </Carousel.Item>
             <Carousel.Item interval={1000}>
-                <CarouselImage imgUrl="../src/assets/carrusel2.png" />
+                <CarouselImage imgUrl="../public/carrusel2.png" />
             </Carousel.Item>
             <Carousel.Item interval={1000}>
-                <CarouselImage imgUrl="../src/assets/carrusel3.png" />
+                <CarouselImage imgUrl="../../carrusel3.png" />
             </Carousel.Item>
             <Carousel.Item interval={1000}>
-                <CarouselImage imgUrl="../src/assets/carrusel4.png" />
+                <CarouselImage imgUrl="../../carrusel4.png" />
             </Carousel.Item>
         </Carousel>
     );
