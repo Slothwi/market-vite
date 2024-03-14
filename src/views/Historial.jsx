548dfc5fd6cb3bbd794cab3e03f06b33340d2535
Slothwi <gabo.leonp@gmail.com>
2024-03-14 13:51:09 -0300

    Act Views

--- src/views/Historial.jsx
+++ src/views/Historial.jsx
@@ -1,11 +1,25 @@
 import { Container } from "react-bootstrap";
+import Accordion from 'react-bootstrap/Accordion';
 
 
 const Historial = () => {
 
     return (
     <Container className="cart">
-        <h1>Historial de compra</h1>
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>Producto #1</Accordion.Header>
+                    <Accordion.Body>
+                        Fecha del Producto Comprado 
+                    </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+                <Accordion.Header>Producto #2</Accordion.Header>
+                <Accordion.Body>
+                    Fecha del producto comprado
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
     </Container>
     );
 };
--- src/views/Publish.jsx
+++ src/views/Publish.jsx
@@ -1,78 +1,97 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { Container } from "react-bootstrap";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Carrousel from '../components/Carrousel';
-import { Ban,ArrowUpFromLine } from 'lucide-react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Container, Button, Form, Row, Col } from 'react-bootstrap';
+import { Ban, ArrowUpFromLine } from 'lucide-react';
 import { NavLink } from 'react-router-dom';
 
 const Publish = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        brand: '',
+        description: '',
+        price: '',
+        stock: '',
+        status: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await axios.post('https://marketplace-back-end-4sb8.onrender.com/api/v1', formData);
+            console.log('Producto publicado exitosamente');
+            // Redireccionamiento o acción adicional después de publicar el producto
+        } catch (error) {
+            console.error('Error al publicar el producto:', error);
+        }
+    };
 
     return (
-    <>
-        <Container className="access">
-            <h2 className='text-white'>Publica Tu Producto</h2>
-        <Form className='text-white'>
-        <Row>
-            <Col>
-                <Form.Group className="mb-3" controlId="formBasicName">
-                    <Form.Label>Nombre del Producto:</Form.Label>
-                    <Form.Control type="name" placeholder="Producto" />
-                </Form.Group>
-            </Col>
-            <Col>
-                <Form.Group className="mb-3" controlId="formBasicBrand">
-                    <Form.Label>Marca del Producto:</Form.Label>
-                    <Form.Control type="brand" placeholder="Marca" />
-                </Form.Group>
-            </Col>
-        </Row>
+        <>
+            <Container className="access">
+                <h2 className='text-white'>Publica Tu Producto</h2>
+                <Form className='text-white' onSubmit={handleSubmit}>
+                    <Row>
+                        <Col>
+                            <Form.Group className="mb-3" controlId="formBasicName">
+                                <Form.Label>Nombre del Producto:</Form.Label>
+                                <Form.Control type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Producto" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="mb-3" controlId="formBasicBrand">
+                                <Form.Label>Marca del Producto:</Form.Label>
+                                <Form.Control type="text" name="brand" value={formData.brand} onChange={handleChange} placeholder="Marca" />
+                            </Form.Group>
+                        </Col>
+                    </Row>
 
-    <Form.Group className="mb-3" controlId="formBasicDescription">
-        <Form.Label>Descripcion del producto:</Form.Label>
-        <Form.Control as="textarea" rows={3} type="Description" placeholder="Descripcion" />
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicPrice">
-        <Form.Label>Precio Del Producto:</Form.Label>
-        <Form.Control type="Price" placeholder="$$$$$" />
-    </Form.Group>
-    <Row>
-            <Col>
-                <Form.Group className="mb-3" controlId="formBasicStock">
-                    <Form.Label>Cantidad:</Form.Label>
-                    <Form.Control type="Stock" placeholder="Stock" />
-                </Form.Group>
-            </Col>
-            <Col>
-                <Form.Group className="mb-3" controlId="formBasicBrand">
-                    <Form.Label>Estado:</Form.Label>
-                    <Form.Select aria-label="Status">
-                        <option>Elige el estado</option>
-                        <option value="1">Nuevo</option>
-                        <option value="2">Semi-Nuevo</option>
-                        <option value="3">Usado</option>
-                    </Form.Select>
-                </Form.Group>
-            </Col>
-        </Row>
-    <div className='buttons'>
-    <NavLink to="/mainpage/home">
-        <Button variant="outline-danger" type="submit">
-            <Ban />Cancelar
-        </Button>
-    </NavLink>
-    <Button variant="outline-light" type="submit">
-        <ArrowUpFromLine />Publicar
-    </Button>
-    </div>
-    </Form>
-    </Container>
-    <Container>
-        <Carrousel  />
-    </Container>
-    </>
+                    <Form.Group className="mb-3" controlId="formBasicDescription">
+                        <Form.Label>Descripcion del producto:</Form.Label>
+                        <Form.Control as="textarea" rows={3} name="description" value={formData.description} onChange={handleChange} placeholder="Descripcion" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPrice">
+                        <Form.Label>Precio Del Producto:</Form.Label>
+                        <Form.Control type="text" name="price" value={formData.price} onChange={handleChange} placeholder="$$$$$" />
+                    </Form.Group>
+                    <Row>
+                        <Col>
+                            <Form.Group className="mb-3" controlId="formBasicStock">
+                                <Form.Label>Cantidad:</Form.Label>
+                                <Form.Control type="text" name="stock" value={formData.stock} onChange={handleChange} placeholder="Stock" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="mb-3" controlId="formBasicBrand">
+                                <Form.Label>Estado:</Form.Label>
+                                <Form.Select aria-label="Status" name="status" value={formData.status} onChange={handleChange}>
+                                    <option>Elige el estado</option>
+                                    <option value="1">Nuevo</option>
+                                    <option value="2">Semi-Nuevo</option>
+                                    <option value="3">Usado</option>
+                                </Form.Select>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <div className='buttons'>
+                        <NavLink to="/mainpage/home">
+                            <Button variant="outline-danger" type="button">
+                                <Ban />Cancelar
+                            </Button>
+                        </NavLink>
+                        <Button variant="outline-light" type="submit">
+                            <ArrowUpFromLine />Publicar
+                        </Button>
+                    </div>
+                </Form>
+            </Container>
+        </>
     );
 };
 
 export default Publish;
+
